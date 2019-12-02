ceb66bf23c9ea2ae55e2e533fc2bc8adec6bb098
Felipe Cooper <felipecardoso323@hotmail.com>
2019-12-01 22:59:15 -0300

    Testando a primeira classe : Categoria

--- src/main/java/com/felipecooper/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/felipecooper/cursomc/resources/CategoriaResource.java
@@ -1,15 +1,26 @@
 package com.felipecooper.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.felipecooper.cursomc.entity.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 
 	@GetMapping
-	public String listar() {
-		return "Tudo funcionando";
+	public List<Categoria> listar() {
+		Categoria categoria1 = new Categoria(3,"Informatica");
+		Categoria categoria2 = new Categoria(5,"Escritorio");
+		List<Categoria> listaCategoria = new ArrayList<>();
+		
+		listaCategoria.add(categoria1);
+		listaCategoria.add(categoria2);
+		return listaCategoria;
 	}
 }
