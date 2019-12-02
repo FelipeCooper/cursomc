7621da7ea5b6940104e57037755315faf83b0e00
Felipe Cooper <felipecardoso323@hotmail.com>
2019-12-01 23:43:04 -0300

    Banco de dados H2

--- src/main/java/com/felipecooper/cursomc/entity/Categoria.java
+++ src/main/java/com/felipecooper/cursomc/entity/Categoria.java
@@ -2,19 +2,26 @@ package com.felipecooper.cursomc.entity;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
+@Getter @Setter
+@NoArgsConstructor @AllArgsConstructor
+@Entity
 public class Categoria implements Serializable {
 	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
-	
+	@NotNull
 	private String nome;
 	
 	@Override
